791896be6ba69bf222a70e76782d6eebebdf1cc7
theo <teddyhoover@gmail.com>
2021-03-01 17:48:17 -0600

    commit tree 94bad1df948373599211b06e1eddceb08285dd50

---
+++ db/migrate/001_create_preferences.rb
@@ -0,0 +1,11 @@
+class CreatePreferences < ActiveRecord::Migration
+    def change
+      create_table :preferences do |t|
+        t.boolean :allow_create_artists
+        t.boolean :allow_create_songs
+  
+        t.timestamps null: false
+      end
+    end
+  end
+  
